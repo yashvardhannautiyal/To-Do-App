364df0ee2390cd90e7b2b13b15dfb43b330a4e6d
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-16 11:50:48 +0530

    edited

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -9,20 +9,18 @@ const ToDo = () => {
 
   // add task function
   const addTask = () => {
-    if (task) {//1
+    if (task) {
       const newTask = {
-        //2
         text: task,
         completed: false,
       };
-      setTasks([...tasks, newTask]); //3
-      setTask(""); //4
+      setTasks([...tasks, newTask]); 
+      setTask(""); 
     }
   };
 
   // delete task function
   const deleteTask = (index) => {
-    //5
     const newTasks = [];
     for (let i = 0; i < tasks.length; i++) {
       if (i !== index) {
@@ -35,10 +33,8 @@ const ToDo = () => {
   // task-completed function
   const taskCompleted = (index) => {
     const newTasks = tasks.map((task, i) => {
-      //6
       if (i == index) {
-        //7
-        return { ...task, completed: !task.completed }; //8
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
@@ -64,7 +60,6 @@ const ToDo = () => {
             placeholder="Add a new task"
             className="border-none bg-slate-100 focus:outline-none rounded-md h-10 pl-2 text-sm"
           />{" "}
-          {/* //5  */}
           <button
             onClick={addTask}
             className="font-extrabold bg-orange-700 rounded-md w-10 h-10 m-4 flex items-center justify-center text-white"
@@ -112,17 +107,4 @@ const ToDo = () => {
 };
 export default ToDo;
 
-// EXPLANATION
-// 1-> if task string is not empty
-// 2-> create a new object with properties : text(value = task input) and completed(boolean value false indicating task is not completed)
-// 3-> creates a new array that includes all existing tasks using spread operator (...tasks)to include each task in the tasks array and adds the new task object (newTask) to the end of this array.
-// 4-> clears the input field so that the user can add another task
-// 5-> delete function creates a new array of newTasks and pushes the task whoose index does not matches with the task the user want to delete from the tasks
-// 6-> tasks.map(...) is an array method that creates a new array by calling a given function
-//task : is the current task
-//i : is the index of the current task
-// 7-> checks if the current index (i) matches the index of the task to be toggled (index)
-// 8-> { ...task } uses the spread operator to create a shallow copy of the current task object.
-///completed: !task.completed updates the completed property to its opposite value (true to false or false to true).
-//This effectively toggles the completed status of the task.
-// 9-> onchange : triggers whenever the value of input field is changed | e : it is the event object passed to the event handler when the event is triggered. | e.target.value : retrieves the current value of the input field.
+
